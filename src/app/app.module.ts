36c297f448d7001fc2c7cfed473a89d1e3acc179
Alexey Svetinskiy <alexey.svetinskiy@cosysoft.ru>
2020-09-10 10:57:45 +0400

    -

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskComponent } from './components/task/task.component';
-import { TaskListComponent } from './components/task-list/task-list.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     TaskComponent,
-    TaskListComponent,
   ],
   imports: [
     BrowserModule,
